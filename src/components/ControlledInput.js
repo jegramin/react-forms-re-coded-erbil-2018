1bc4f283d72a0838e54b723e8e04a83068eb227e
Mohammed Ameen <jegroamin@gmail.com>
2018-09-13 13:38:44 +0000

    automatically backed up by learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -8,6 +8,9 @@ class Form extends Component {
   }
   <input type="text" id="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
 <input type="text" id="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+
+<input type="text" id="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+<input type="text" id="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
  
   render() {
     return (
